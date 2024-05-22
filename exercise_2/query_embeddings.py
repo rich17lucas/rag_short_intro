455fd12a75f7f494acdabbbf128a02ce181c76d1
rich17lucas <richard@rnslucas.com>
2024-05-22 17:36:53 +0100

    FORMATTING: Black linter changes

--- exercise_2/query_embeddings.py
+++ exercise_2/query_embeddings.py
@@ -8,28 +8,22 @@ from openai import OpenAI
 # Store your env variable in .env for PINECONE_API_KEY
 load_dotenv()
 oa = OpenAI()
-pc = Pinecone(api_key=os.getenv('PINECONE_API_KEY'))
-
+pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 
 
 # Function to embed text, it will be used
 # to change the query text to a vector
 def get_embedding_for(text):
 
-    response = oa.embeddings.create(
-        model="text-embedding-3-small",
-        input=text
-
-    )
+    response = oa.embeddings.create(model="text-embedding-3-small", input=text)
 
     return response.data[0].embedding
-
 
 
 # This function queries the vector database
 def query_questions(question):
 
-    index = pc.Index('questions')
+    index = pc.Index("questions")
 
     # You should notice there are now 10 vectors there
     print(index.describe_index_stats())
@@ -38,16 +32,12 @@ def query_questions(question):
     # TODO complete the following line of code and replace x...
     embedding = x_call_to_embed_text
 
-    results = index.query(
-        vector=embedding,
-        top_k=5,
-        include_metadata=True
-    )
+    results = index.query(vector=embedding, top_k=5, include_metadata=True)
 
     return results
 
 
 # Call the query questions
-results = query_questions('How do I get a taxi in Boston?')
-print('Results response')
+results = query_questions("How do I get a taxi in Boston?")
+print("Results response")
 print(results)
